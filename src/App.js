86efca00fb4f49241306b783ab19e8aa65e8174a
braydenmackay <braydenmackay1@gmail.com>
2019-10-14 11:30:47 -0600

    dafd

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    fetch("http://localhost:5000/todos")
+    fetch("https://fierce-harbor-62939.herokuapp.com/todos/")
       .then(response => response.json())
       .then(data => this.setState({ todos: data }))
   }
@@ -38,7 +38,7 @@ class App extends React.Component {
 
   addTodo = event => {
     event.preventDefault()
-    fetch("http://localhost:5000/todo", {
+    fetch("https://fierce-harbor-62939.herokuapp.com/todo", {
       method: "POST",
       headers: { "content-type": "application/json" },
       body: JSON.stringify({
@@ -56,7 +56,7 @@ class App extends React.Component {
   }
 
   deleteTodo = id => {
-    fetch(`http://localhost:5000/todo/${id}`, {
+    fetch(`https://fierce-harbor-62939.herokuapp.com/todo/${id}`, {
       method: "DELETE"
     }).then(
       this.setState({
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -10,7 +10,7 @@ class TodoItem extends React.Component {
   }
 
   toggleDone = () => {
-    fetch(`http://localhost:5000/todo/${this.props.id}`, {
+    fetch(`https://fierce-harbor-62939.herokuapp.com/todo/${this.props.id}`, {
       method: "PUT",
       headers: { "content-type": "application/json" },
       body: JSON.stringify({
